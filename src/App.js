39681406adb6b2af03246e7e3f447445781221f7
Elijah Paminsan <e.paminsan@gmail.com>
2020-02-29 16:09:23 -0800

    adam-david > master

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BusinessProfile from './components/BusinessProfile';
+
+//components
+
+//styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Replate </h1>
+    </div>
+  );
+}//end App
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//components
+import App from './App';
+
+//styles
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
