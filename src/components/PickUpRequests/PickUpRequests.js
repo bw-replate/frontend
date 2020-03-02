4ffcb25201edbdf5af792aa416bc511df78b6b58
fuston05 <fuston05@yahoo.com>
2020-03-02 09:56:33 -0500

    huh

---
+++ src/components/PickUpRequests/PickUpRequests.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//styles
+import './PickUpRequestsStyles';
+
+const PickUpRequests = () => {
+  return (
+    <div>
+      <h1> Pick Up Requests Here</h1>
+    </div>
+  )
+}// end PickUpRequests
+
+export default PickUpRequests;
