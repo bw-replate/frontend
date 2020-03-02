8d06db7295ae65b247858382d36b76ce39454cab
fuston05 <fuston05@yahoo.com>
2020-03-02 10:00:32 -0500

    added more comments to pickuprequests

--- src/components/PickupRequest/PickupRequest.js
+++ src/components/PickupRequest/PickupRequest.js
@@ -2,7 +2,7 @@
 // - `amount` of food item by count/weight: Integer
 // - `preferredPickupTime`: datetime
 
-//need assigned and unassigned pick up requests.. 2 different components???
+//need assigned and unassigned pick up requests.. 2 different components??? or just style active ones different is probably better??
 
 import React from 'react';
 
